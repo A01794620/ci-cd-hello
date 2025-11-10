5d5fa9fcc16c8cf2718b1f277929111ec30aade8
Ronald Sandí <A01794620@tec.mx>
2025-11-10 12:43:29 -0600

    Gestión de Imagen para Contenerización (G57):Av-123

--- src/main.py
+++ src/main.py
@@ -1,33 +1,36 @@
+import signal
+import threading
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
 import pickle
 import uvicorn
-from sklearn.datasets import load_wine
+from sklearn.datasets import (load_wine)
+import sys
+import os
+import time
 
-model = None
+app = FastAPI()
 
-# Recuperar el modelo
-#with open("../model_pkl/wine_model_.pkl", "rb") as f:
-with open("model_pkl/wine_model_.pkl", "rb") as f:
-    model = pickle.load(f)
 
-data = load_wine()
-target_names = data.target_names
+def server_mld():
+    model = None
+    # Recuperar el modelo
+    #with open("../model_pkl/wine_model_.pkl", "rb") as f:
+    with open("model_pkl/wine_model_.pkl", "rb") as f:
+        model = pickle.load(f)
+
+    data = load_wine()
+    target_names = data.target_names
 
 
 class WineData(BaseModel):
     features: List[float]
 
 
-app = FastAPI()
-
-
 @app.post("/predict-nish")
 def predict(wine_data: WineData):
-
     if len(wine_data.features) != model.n_features_in_:
-
         raise HTTPException(
             status_code=400,
             detail="The input data has not the right number of features.",
@@ -44,5 +47,23 @@ def read_root():
     return {"message": "OK Yeah!"}
 
 
-if __name__ == "__main__":
+def run_uvicorn():
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    just_build = False
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "just_build":
+            just_build = True
+    if just_build:
+        uvicorn_thread = threading.Thread(target=run_uvicorn)
+        uvicorn_thread.start()
+        time.sleep(5)
+        os.kill(os.getpid(), signal.SIGINT)
+        uvicorn_thread.join()
+        print("API Built Successfully")
+    else:
+        run_uvicorn()
+        print("Serving API Successfully")
